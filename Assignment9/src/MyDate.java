82e4ec42f6f6e676def7b3b6a506afe296d6ceb1
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2023-07-19 17:51:10 -0400

    Add files via upload

---
+++ Assignment9/src/MyDate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyDate {
+	int year, month, day;
+
+	public MyDate() {
+		
+	}
+	
+	public int 
+	public static void main(String[] args) {
+		MyDate currentDate = new MyDate();
+		
+	}
+}
